99d514cf8214f028adad4710b5c40dc3f8830a8b
smetehan <s.metehan.parlak@gmail.com>
2023-01-08 16:00:55 +0300

    pagination örnek

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(3);
+
+  const showMoreMeals = () => {
+    setVisible((item) => (item += 3));
+  };
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=british")
+      .then((res) => res.json())
+      .then((data) => setItems(data.meals));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {" "}
+        <h1 className="header">British Meal </h1>
+        <img
+          className="imgtitle"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1200px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png"
+        />
+      </div>
+
+      <div className="container">
+        {items?.slice(visible - 3, visible).map((item) => {
+          const { strMealThumb, strMeal, idMeal } = item;
+          return (
+            <div className="card" key={idMeal}>
+              <div className="image">
+                <img src={strMealThumb} />
+              </div>
+              <p>{strMeal}</p>
+            </div>
+          );
+        })}
+        <button onClick={showMoreMeals}>Click for More Meals</button>
+      </div>
     </div>
   );
 }
